865426a0482233199b12f96524738191be1e89ca
JCPrime01 <vanzokiller@gmail.com>
2026-05-30 21:36:28 -0300

    chore: shareCode 0V180N899XP -> ZSOBJP7PKYM (via painel)

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=0V180N899XP&afp=ti' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/63ycdffd?shareCode=ZSOBJP7PKYM&afp=ti' });
     res.end();
   }
